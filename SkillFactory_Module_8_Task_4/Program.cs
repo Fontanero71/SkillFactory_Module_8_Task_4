4a7f8f45c3ac065ae8e9babb1d280376a2c4e482
Сергей Чекалин <schekalin@mail.ru>
2022-05-07 20:19:19 +0300

    Добавьте файлы проекта.

---
+++ SkillFactory_Module_8_Task_4/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace SkillFactory_Module_8_Task_4
+{
+    
+    internal class Program
+    {
+        [Serializable]
+        public class Student
+        {
+            public string Name { get; set; }
+            public string Group { get; set; }
+            public DateTime DateTime { get; set; }
+            public Student(string name, string group, DateTime dateTime)
+            {
+                Name = name;
+                Group = group;
+                DateTime = dateTime;
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            string dir = @"C:\Users\schek\Desktop\Students";
+
+            if (!Directory.Exists(dir))
+                Directory.CreateDirectory(dir);
+
+            string path = @"C:\Users\schek\Desktop\Students.dat";
+
+            
+            BinaryFormatter formatter = new BinaryFormatter();
+
+            using (var fs = new FileStream(path, FileMode.OpenOrCreate))
+            {
+                Student[] newS = (Student[])formatter.Deserialize(fs);
+                foreach (var s in newS)
+                {
+                    Console.WriteLine(s.Name);
+                    Console.WriteLine(s.Group);
+                    Console.WriteLine(s.DateTime);
+                }
+
+            }
+
+
+        }
+    }
+}
